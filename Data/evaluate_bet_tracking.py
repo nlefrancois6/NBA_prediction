dd8a517f1fcc6945524e8c70451fcf43989adb90
Noah LeFrancois <49576860+nlefrancois6@users.noreply.github.com>
2021-01-15 00:04:34 -0500

    Upload all of the data files

---
+++ Data/evaluate_bet_tracking.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  9 13:05:48 2021
+
+@author: noahlefrancois
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#bet_tracking_df = pd.read_csv('bet_tracking2021_originalModel.csv')
+bet_tracking_df = pd.read_csv('bet_tracking2021_optModel.csv')
+
+
+bet_success = []
+for i in range(len(bet_tracking_df)):
+    if bet_tracking_df['Recommended Winner'].iloc[i] == bet_tracking_df['Winner'].iloc[i]:
+        bet_success.append('G')
+    else:
+        bet_success.append('L')
+    
+bet_tracking_df['Bet Success'] = bet_success
+
+winner_actual = bet_tracking_df['Winner']
+winner_prediction = bet_tracking_df['Recommended Winner']
+wagers = bet_tracking_df['Recommended Wager']
+v_odds = bet_tracking_df['V ML']
+h_odds = bet_tracking_df['H ML']
+
+
+account = 100
+account_runningTotal = [account]
+gains_store = []
+gain = 0
+
+numGames = len(winner_prediction)
+for i in range(numGames):
+    wager = wagers[i]
+    #wager = 10
+    #If our prediction was correct, calculate the winnings
+    if winner_actual[i] == winner_prediction[i]:
+        #wins += 1
+        if winner_prediction[i] == 'V':
+            #odds[0] is odds on visitor win
+            if v_odds[i]>0:
+                gain = v_odds[i]*(wager/100)
+            else:
+                gain = 100*(wager/(-v_odds[i]))
+        if winner_prediction[i] == 'H':
+            #odds[1] is odds on home win
+            if h_odds[i]>0:
+                gain = h_odds[i]*(wager/100)
+            else:
+                gain = 100*(wager/(-h_odds[i]))
+
+    #If our prediction was wrong, lose the wager
+    else:
+        #losses += 1
+        gain = -wager
+    
+    account = account + gain
+    account_runningTotal.append(account)
+    gains_store.append(gain)
+
+numGames_bet = len(account_runningTotal)
+print('Final account balance after ', numGames_bet, ' bets: ', account)
+
+plt.figure()
+plt.plot(account_runningTotal[:])
+plt.title('Model Performance To Date (2020-2021 Season)')
+plt.xlabel('Number of Games')
+plt.ylabel('Account Balance')
+plt.hlines(account_runningTotal[0], 0, len(account_runningTotal),linestyles='dashed')
+
+hist = False
+if hist:
+    plt.figure()
+    plt.hist(gains_store)
+
+
---
+++ Data/odds_scraper.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 21:41:38 2021
+
+@author: noahlefrancois
+"""
+
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import requests
+from pandas import DataFrame
+import time
+from datetime import date
+
+#url = 'https://www.tipico.de/de/live-wetten/'
+url = 'https://www.sportsbookreview.com/betting-odds/nba-basketball/money-line/?date=20151027'
+
+try:
+    page = urllib.request.urlopen(url)
+except:
+    print('An error occured.')
+    
+soup = BeautifulSoup(page, 'html.parser')
+#print(soup)
+   
+regex = re.compile('wrapper-2tuQL')
+content_lis = soup.find_all('button', attrs={'class': regex})
+print(content_lis)
+
+content = []
+for li in content_lis:
+    content.append(li.getText().replace('\n','').replace('\t',''))
+print(content)
+
+"""
+def soup_url(type_of_line, tdate):
+## get html code for odds based on desired line type and date
+    web_url = dict()
+    web_url['money-line'] = ''
+    url_addon = web_url[type_of_line]
+    #url = 'http://www.sportsbookreview.com/betting-odds/tennis/' + url_addon + '?date=' + tdate
+    #https://www.sportsbookreview.com/betting-odds/nba-basketball/money-line/?date=20151027
+    url = 'http://www.sportsbookreview.com/betting-odds/nba-basketball/' + url_addon + '?date=' + tdate
+    raw_data = requests.get(url)
+    soup= BeautifulSoup(raw_data.text, 'html.parser')
+    timestamp = time.strftime("%H:%M:%S")
+    return soup, timestamp
+
+    '''
+    BookID  BookName
+	238 Pinnacle Sports
+	349 Will Hill
+	43 bet365
+	93 Bookmaker
+	1096 BetOnline
+	227 The Greek Sportsbook
+	1275 JustBet
+	300 SportsInteraction
+	999993 WagerWeb
+	19 5Dimes
+	92 Bodog
+	999991 Sportsbetting
+	999998 SBR Sportsbook
+	1537 Sportbet
+	442 IslandCasino
+	123 Diamond Sportsbook
+	180 Intertops
+	118 betcris
+	169 Heritage
+	139 YouWager
+	186 JazzSports
+	23 ABCislands
+	423 LooseLines
+	83 BetUs
+	626 matchbook
+	999996 Bovada
+	1602 GtBets
+	1680 Mybookie.ag
+    '''
+
+
+def replace_unicode(string):
+    return string.replace(u'\xa0',' ').replace(u'\xbd','.5')
+
+def parse_and_write_data(soup, date, time):
+## Parse HTML to gather line data by book
+    def book_line(book_id, line_id, homeaway):
+        ## Get Line info from book ID
+        line = soup.find_all('div', attrs = {'class':'el-div eventLine-book', 'rel':book_id})[line_id].find_all('div')[homeaway].get_text().strip()
+        return line
+    if True:
+        df = DataFrame(
+                columns=(
+     'player','Pinnacle','Will Hill','bet365','Bookmaker','BetOnline','The Greek Sportsbook','JustBet','SportsInteraction','WagerWeb','FiveDimes'))
+
+    counter = 0
+    number_of_games = len(soup.find_all('div', attrs = {'class':'el-div eventLine-rotation'})) #correct
+    players = soup.find_all('div', attrs = {'class':'el-div eventLine-team'})
+    for i in range(0, number_of_games):
+        A = []
+        H = []
+        ## get line/odds info for unique book. Need error handling to account for blank data
+        def try_except_book_line(id, i , x):
+            try:
+                return book_line(id, i, x)
+            except IndexError:
+                return ''
+        player_A = players[i].find_all('div')[0].get_text().strip()
+        pinnacle_A = 0 #try_except_book_line('238',i, 0)
+        willhill_A = try_except_book_line('349',i, 0)
+        betThreeSixFive_A = try_except_book_line('43',i, 0)
+        bookmaker_A = try_except_book_line('93',i, 0)
+        betonline_A = try_except_book_line('1096', i, 0)
+        thegreeksportsbook_A = try_except_book_line('227',i, 0)
+        justbet_A = try_except_book_line('1275',i, 0)
+        sportsinteraction_A = try_except_book_line('300',i, 0)
+        wagerweb_A = try_except_book_line('999993',i, 0)
+        fivedimes_A = try_except_book_line('19',i, 0)
+
+        player_H = players[i].find_all('div')[1].get_text().strip()
+
+        pinnacle_H = 0 #try_except_book_line('238',i, 1)
+        willhill_H = try_except_book_line('349',i, 1)
+        betThreeSixFive_H = try_except_book_line('43',i, 1)
+        bookmaker_H = try_except_book_line('93',i, 1)
+        betonline_H = try_except_book_line('1096', i, 1)
+        thegreeksportsbook_H = try_except_book_line('227',i, 1)
+        justbet_H = try_except_book_line('1275',i, 1)
+        sportsinteraction_H = try_except_book_line('300',i, 1)
+        wagerweb_H = try_except_book_line('999993',i, 1)
+        fivedimes_H = try_except_book_line('19',i, 1)
+
+        if ") " in replace_unicode(player_A):
+          A.append(str(replace_unicode(player_A)).lower().split(") ")[1])
+        else:
+          A.append(str(replace_unicode(player_A)).lower())
+        A.append(str(pinnacle_A))
+        A.append(str(willhill_A))
+        A.append(str(betThreeSixFive_A))
+        A.append(str(bookmaker_A))
+        A.append(str(betonline_A))
+        A.append(str(thegreeksportsbook_A))
+        A.append(str(justbet_A))
+        A.append(str(sportsinteraction_A))
+        A.append(str(wagerweb_A))
+        A.append(str(fivedimes_A))
+
+        if ") " in replace_unicode(player_H):
+          H.append(str(replace_unicode(player_H)).lower().split(") ")[1])
+        else:
+          H.append(str(replace_unicode(player_H)).lower())
+        H.append(str(pinnacle_H))
+        H.append(str(willhill_H))
+        H.append(str(betThreeSixFive_H))
+        H.append(str(bookmaker_H))
+        H.append(str(betonline_H))
+        H.append(str(thegreeksportsbook_H))
+        H.append(str(justbet_H))
+        H.append(str(sportsinteraction_H))
+        H.append(str(wagerweb_H))
+        H.append(str(fivedimes_H))
+        ## Take data from A and H (lists) and put them into DataFrame
+        df.loc[counter]   = ([A[j] for j in range(len(A))])
+        df.loc[counter+1] = ([H[j] for j in range(len(H))])
+        counter += 2
+    return df
+
+todays_date = str(date.today()).replace('-','')
+soup, time_ml = soup_url('money-line', todays_date)
+df = parse_and_write_data(soup,todays_date,time_ml)
+"""
+"""
+url = 'https://www.sportsbookreview.com/betting-odds/nfl-football/consensus/'
+res = requests.get(url)
+soup = BeautifulSoup(res.content, "lxml")
+a = []
+for item in soup.find_all('div', {'class': '_3h0tU'}):
+   for div in item.find_all('div'):
+      a.append(div.text)
+      print (div.text)
+"""
+
+
